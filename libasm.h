3e6bdf7329ca4bd6932d54ff099204c5406517a3
Roney <roneyrogerio92@gmail.com>
2020-08-18 21:39:17 -0300

    lib header

---
+++ libasm.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rde-oliv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/03 16:45:55 by rde-oliv          #+#    #+#             */
+/*   Updated: 2020/08/05 13:59:32 by rde-oliv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBASM_H
+# define LIBASM_H
+# include <stddef.h>
+# include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strcpy(char *dest, const char *src);
+int		ft_strcmp(const char *s1, const char *s2);
+ssize_t	ft_write(int fd, const void *buf, size_t count);
+ssize_t	ft_read(int fd, void *buf, size_t count);
+char	*ft_strdup(const char *s);
+
+#endif
