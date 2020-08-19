871c79c9ddd0c4f341fc99183af17298a03975e2
Roney <roneyrogerio92@gmail.com>
2020-08-18 21:36:17 -0300

    New function to write to a file descriptor.

---
+++ ft_write.s
@@ -0,0 +1,21 @@
+segment .text
+	global ft_write
+	extern __errno_location
+
+ft_write:
+	mov r8, rdx
+	mov rax, 0x2000004
+	syscall
+	cmp rax, 0
+	jl .error
+	mov rax, r8
+	ret
+
+.error:
+	push rax
+	call __errno_location
+	pop rbx
+	neg rbx
+	mov [rax], rbx
+	mov rax, -1
+	ret
